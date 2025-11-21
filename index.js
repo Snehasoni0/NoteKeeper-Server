4dd3b448aac77882be855ea933c808eba0d33f11
Sneha Soni <baberwalsnehaa@gmail.com>
2025-11-21 20:18:51 +0530

    minor changes

--- index.js
+++ index.js
@@ -9,13 +9,26 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: [
+    'https://note-keeper-frontend-alpha.vercel.app/',
+    'http://localhost:5173'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.options('*', cors(corsOptions));
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 app.use("/api/auth", Authrouter);
 app.use("/api/note", NoteRouter);
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
   res.send("API running..!");
 })
 
